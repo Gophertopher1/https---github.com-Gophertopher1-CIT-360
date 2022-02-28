b64d8d8849861e455d1ee30a00154486301d29fe
Gophertopher1 <mcgill.chris1@gmail.com>
2022-02-27 17:23:49 -0700

    Junit

---
+++ src/Higher.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertequals;
+
+import org.junit.Test;
+
+/**
+ * Higher
+ */
+public class Higher {
+
+    public static void main(String[] args) {
+        int a = 25;
+        int b = -25;
+        int c = higher(a, b);
+        System.out.println("The higher of 25 and -25 is " + c);
+    }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return the higher of the two values
+     */
+    public static int higher(int x, int y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    @test
+    public void testHigher() {
+        assertEquals(25, higher(25, 25));
+        assertEquals(5, higher(3, 5));
+    }
+
+}
